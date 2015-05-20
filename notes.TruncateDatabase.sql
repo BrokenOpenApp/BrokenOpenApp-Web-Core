c3b11d7487ede1eacb96753a6d4e75c1500dc613
James Baster <james@jarofgreen.co.uk>
2015-05-20 10:53:59 +0100

    Notes

---
+++ notes.TruncateDatabase.sql
@@ -0,0 +1,19 @@
+
+DELETE FROM boa_settings_system  ;
+DELETE FROM boa_crash_build    ;
+DELETE FROM boa_crash_crash_configuration ;
+DELETE FROM boa_crash_display  ;
+DELETE FROM boa_crash_environment   ;
+DELETE FROM boa_crash_features   ;
+DELETE FROM boa_crash_initial_configuration ;
+DELETE FROM boa_crash_settings_global  ;
+DELETE FROM boa_crash_settings_secure  ;
+DELETE FROM boa_crash_shared_preferences;
+DELETE FROM boa_crash          ;
+DELETE FROM boa_issue ;
+DELETE FROM boa_incoming_crash_acra  ;
+DELETE FROM boa_user_in_project ;
+DELETE FROM boa_project  ;
+
+DELETE FROM boa_user ;
+
