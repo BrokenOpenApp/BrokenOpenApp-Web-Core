243c8b3caa4bcef4d964571eb580caf4145f8f4a
James Baster <james@jarofgreen.co.uk>
2015-05-21 09:19:20 +0100

    Added table

--- notes.TruncateDatabase.sql
+++ notes.TruncateDatabase.sql
@@ -10,6 +10,7 @@ DELETE FROM boa_crash_settings_global  ;
 DELETE FROM boa_crash_settings_secure  ;
 DELETE FROM boa_crash_shared_preferences;
 DELETE FROM boa_crash          ;
+DELETE FROM boa_issue_history_title ;
 DELETE FROM boa_issue ;
 DELETE FROM boa_incoming_crash_acra  ;
 DELETE FROM boa_user_in_project ;
