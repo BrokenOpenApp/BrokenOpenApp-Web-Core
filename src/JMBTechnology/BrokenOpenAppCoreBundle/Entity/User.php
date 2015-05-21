fc00ea4c9dcc1ef8e57ed0d9401a26b5ba04f651
James Baster <james@jarofgreen.co.uk>
2015-05-21 11:41:10 +0100

    Users - set up way to lock accounts closes https://github.com/BrokenOpenApp/BrokenOpenApp-Web-Core/issues/21

--- src/JMBTechnology/BrokenOpenAppCoreBundle/Entity/User.php
+++ src/JMBTechnology/BrokenOpenAppCoreBundle/Entity/User.php
@@ -3,7 +3,7 @@
 namespace JMBTechnology\BrokenOpenAppCoreBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -15,7 +15,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="JMBTechnology\BrokenOpenAppCoreBundle\Entity\UserRepository")
  */
-class User  implements UserInterface, \Serializable
+class User  implements AdvancedUserInterface, \Serializable
 {
 
 	/**
@@ -51,6 +51,13 @@ class User  implements UserInterface, \Serializable
 	 * @ORM\Column(name="is_create_project", type="boolean", nullable=false)
 	 */
 	private $is_create_project = false;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="is_locked", type="boolean", nullable=false)
+	 */
+	private $is_locked = false;
 
 	public function __construct()
 	{
@@ -157,6 +164,22 @@ class User  implements UserInterface, \Serializable
 		$this->is_super_admin = $is_super_admin;
 	}
 
+	/**
+	 * @return string
+	 */
+	public function getIsLocked()
+	{
+		return $this->is_locked;
+	}
+
+	/**
+	 * @param string $is_locked
+	 */
+	public function setIsLocked($is_locked)
+	{
+		$this->is_locked = $is_locked;
+	}
+
 
 
 
@@ -168,7 +191,8 @@ class User  implements UserInterface, \Serializable
 			$this->email,
 			$this->password,
 			$this->is_create_project,
-			$this->is_super_admin
+			$this->is_super_admin,
+			$this->is_locked
 		));
 	}
 
@@ -180,8 +204,70 @@ class User  implements UserInterface, \Serializable
 			$this->email,
 			$this->password,
 			$this->is_create_project,
-			$this->is_super_admin
+			$this->is_super_admin,
+			$this->is_locked
 			) = unserialize($serialized);
 	}
+
+	/**
+	 * Checks whether the user's account has expired.
+	 *
+	 * Internally, if this method returns false, the authentication system
+	 * will throw an AccountExpiredException and prevent login.
+	 *
+	 * @return bool true if the user's account is non expired, false otherwise
+	 *
+	 * @see AccountExpiredException
+	 */
+	public function isAccountNonExpired()
+	{
+		return true;
+	}
+
+	/**
+	 * Checks whether the user is locked.
+	 *
+	 * Internally, if this method returns false, the authentication system
+	 * will throw a LockedException and prevent login.
+	 *
+	 * @return bool true if the user is not locked, false otherwise
+	 *
+	 * @see LockedException
+	 */
+	public function isAccountNonLocked()
+	{
+		return !$this->is_locked;
+	}
+
+	/**
+	 * Checks whether the user's credentials (password) has expired.
+	 *
+	 * Internally, if this method returns false, the authentication system
+	 * will throw a CredentialsExpiredException and prevent login.
+	 *
+	 * @return bool true if the user's credentials are non expired, false otherwise
+	 *
+	 * @see CredentialsExpiredException
+	 */
+	public function isCredentialsNonExpired()
+	{
+		return true;
+	}
+
+	/**
+	 * Checks whether the user is enabled.
+	 *
+	 * Internally, if this method returns false, the authentication system
+	 * will throw a DisabledException and prevent login.
+	 *
+	 * @return bool true if the user is enabled, false otherwise
+	 *
+	 * @see DisabledException
+	 */
+	public function isEnabled()
+	{
+		return true;
+	}
+
 
 }
