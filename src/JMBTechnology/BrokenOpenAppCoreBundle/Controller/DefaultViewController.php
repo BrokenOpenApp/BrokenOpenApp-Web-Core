8a07f30f32a6fb7c4c9b1f2f8767f3ce52b6798b
James Baster <james@jarofgreen.co.uk>
2015-05-26 22:22:45 +0100

    Remove unused code

--- src/JMBTechnology/BrokenOpenAppCoreBundle/Controller/DefaultViewController.php
+++ src/JMBTechnology/BrokenOpenAppCoreBundle/Controller/DefaultViewController.php
@@ -7,16 +7,14 @@ use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-use JMBTechnology\BrokenOpenAppCoreBundle\Controller\IAcraServerController;
 use JMBTechnology\BrokenOpenAppCoreBundle\Entity\Crash;
-use JMBTechnology\BrokenOpenAppCoreBundle\DataFixtures\LoadFixtureData;
 
 
 /**
  * @license Apache Open Source License 2.0 http://www.apache.org/licenses/LICENSE-2.0
  * @link http://www.brokenopenapp.org/ BrokenOpenApp Home Page for docs and support
  */
-class DefaultViewController extends Controller implements IAcraServerController
+class DefaultViewController extends Controller
 {
 
     /**
