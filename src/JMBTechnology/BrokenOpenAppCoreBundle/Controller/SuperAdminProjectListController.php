73cfb01dd66cdb303d05bdf4fff1800af01ebe7d
James <james@jarofgreen.co.uk>
2015-06-10 15:07:16 +0100

    Super Admin Projects List

---
+++ src/JMBTechnology/BrokenOpenAppCoreBundle/Controller/SuperAdminProjectListController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JMBTechnology\BrokenOpenAppCoreBundle\Controller;
+
+
+
+/**
+ * @license Apache Open Source License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * @link http://www.brokenopenapp.org/ BrokenOpenApp Home Page for docs and support
+ */
+class SuperAdminProjectListController extends DefaultViewController
+{
+
+	public function indexAction()
+	{
+
+		$doctrine = $this->getDoctrine()->getManager();
+
+		$pRepo = $doctrine->getRepository('JMBTechnologyBrokenOpenAppCoreBundle:Project');
+		$projects = $pRepo->findBy(array());
+
+		return $this->render('JMBTechnologyBrokenOpenAppCoreBundle:SuperAdminProjectList:index.html.twig', array(
+			'projects'=>$projects,
+		));
+
+
+	}
+
+
+}
